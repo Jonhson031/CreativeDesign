72d0809a03ce2715065925f61166a477c288b23d
Jonhson031 <88057231+Jonhson031@users.noreply.github.com>
2021-08-04 16:53:01 +0300

    Add files via upload

---
+++ scripts/sricpt.js
@@ -0,0 +1,56 @@
+// Бургер
+$(document). ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger, .header__navigation').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+$(document). ready(function() {
+	$('.header__navigation').click(function(){
+       $('.header__burger, .header__navigation').removeClass('active');
+       $('body').removeClass('lock');
+	});
+});
+
+// Плавний скрол к блоку
+$(document).ready(function() {
+  $("a.header__link").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
+
+// карта
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+// анимация при скроле
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.container, .header, .features__column, .news__bottom, .map');
+for (let elm of elements) {
+  observer.observe(elm);
+}
